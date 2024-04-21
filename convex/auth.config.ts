d0438ddd8819a28aa1c15926f736437df0c3d1a7
David Muriithi <muriithid05@gmail.com>
2024-04-21 12:51:54 +0300

    Add configuration files and initial code for Next.js project

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+        {
+            domain: "https://active-tick-72.clerk.accounts.dev",
+            applicationID: "convex",
+        }
+    ]
+}
+
+export default authConfig;
