5fcfdfcc3e80bcf4f11bb2af91b99e2447b14495
barrios <muriithid5@gmaail.com>
2024-06-14 13:13:00 +0300

    refactor: Move convex schema.ts into the root convex directory where it should be by default.

--- convex/auth.config.ts
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-  providers: [
-    {
-      domain: "https://active-tick-72.clerk.accounts.dev",
-      applicationID: "convex",
-    },
-  ],
-};
