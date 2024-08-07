4b65cbc55323418544293f172747966783330a77
David Muriithi <muriithid05@gmail.com>
2024-08-07 11:42:32 +0300

    comlete ui for AddFriendDialogue

--- src/app/(root)/friends/_components/AddFriendDialogue.tsx
+++ src/app/(root)/friends/_components/AddFriendDialogue.tsx
@@ -1,13 +1,96 @@
-import React from 'react'
+import React from "react";
+import { TypeOf, z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogFooter,
+} from "@/components/ui/dialog";
+import { Tooltip, TooltipTrigger } from "@/components/ui/tooltip";
+import { Button } from "@/components/ui/button";
+import { DialogTrigger } from "@/components/ui/dialog";
+import { UserPlus } from "lucide-react";
+import { TooltipContent } from "@radix-ui/react-tooltip";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
-type Props = {}
+type Props = {};
+
+const AddFriendSchema = z.object({
+  email: z
+    .string()
+    .min(1, { message: "This field can't be empty!" })
+    .email("Please enter a valid email"),
+});
 
 const AddFriendDialogue = (props: Props) => {
+  const form = useForm<z.infer<typeof AddFriendSchema>>({
+    resolver: zodResolver(AddFriendSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  const handleSubmit = () => {};
   return (
-    <div>AddFriendDialogue</div>
-    
-  )
-}
+    <Dialog>
+      <Tooltip>
+        <TooltipTrigger>
+          <Button size="icon" variant="outline">
+            <DialogTrigger>
+              <UserPlus />
+            </DialogTrigger>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Add Friend</p>
+        </TooltipContent>
+      </Tooltip>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Add Friend</DialogTitle>
+          <DialogDescription>Send request to connect</DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleSubmit)}
+            className="space-y-8"
+          >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            ></FormField>
+          </form>
+        </Form>
+        <DialogFooter>
+          <Button size="sm" variant="outline">
+            Cancel
+          </Button>
+          <Button size="sm">Send Request</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
 
-export default AddFriendDialogue
-
+export default AddFriendDialogue;
--- src/app/(root)/friends/page.tsx
+++ src/app/(root)/friends/page.tsx
@@ -1,12 +1,15 @@
+"use client"
+
 import ConversationFallback from "@/components/ui/shared/conversation/ConversationFallback";
 import ItemsList from "@/components/ui/shared/itemlist/itemlist";
+import AddFriendDialogue from "./_components/AddFriendDialogue";
 
 const Props = {};
 
 const FriendsPage: React.FC = () => {
   return (
     <>
-      <ItemsList title="Friends"></ItemsList>
+      <ItemsList title="Friends" action={<AddFriendDialogue />}></ItemsList>
       <ConversationFallback />
     </>
   );
