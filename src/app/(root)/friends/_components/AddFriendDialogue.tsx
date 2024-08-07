81e9d48dcb1b02517139831eaa7a8cea196a26ff
David Muriithi <muriithid05@gmail.com>
2024-08-07 10:58:10 +0300

    modified:   bun.lockb 	modified:   convex/_utils.ts 	modified:   package.json 	new file:   src/app/(root)/friends/_components/AddFriendDialogue.tsx 	new file:   src/components/ui/dialog.tsx     new file:   src/components/ui/form.tsx 	new file:   src/components/ui/input.tsx 	new file:   src/components/ui/label.tsx

---
+++ src/app/(root)/friends/_components/AddFriendDialogue.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const AddFriendDialogue = (props: Props) => {
+  return (
+    <div>AddFriendDialogue</div>
+    
+  )
+}
+
+export default AddFriendDialogue
+
